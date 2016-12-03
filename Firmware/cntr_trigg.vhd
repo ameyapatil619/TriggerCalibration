8a5b69de0a9bb3b8c17de63121d66d7a04b5a167
ameyapatil619 <ameyap@uci.edu>
2016-12-03 00:03:29 -0800

    Synthesizable version of an 8 channel trigger counter

--- Firmware/cntr_trigg.vhd
+++ Firmware/cntr_trigg.vhd
@@ -22,15 +22,6 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 use IEEE.numeric_std.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity cntr_trigg is
 
 generic(N : integer := 32);
